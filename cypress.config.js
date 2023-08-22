361abf2bdfe0e4d7f981428672197f80c57f0839
dbquiroga <day@xcapit.com>
2023-08-22 10:10:00 -0300

    start project

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  env: {
+    baseUrl: "https://www.demoblaze.com/index.html",
+  },
+
+  chromeWebSecurity: true,
+  watchForFileChanges: false,
+});
